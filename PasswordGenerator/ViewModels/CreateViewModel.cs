34d5a58e90e9f8d242615681e25e5b48d11e040b
Evan <evan@ix.co.za>
2025-11-02 06:39:18 +0200

    Working on password delivery page

---
+++ PasswordGenerator/ViewModels/CreateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PasswordGenerator.ViewModels
+{
+	public class CreateViewModel
+	{
+		public int Length { get; set; }
+		public bool SpecialChars { get; set; }
+		public bool UpperCase { get; set; }
+		public bool LowerCase { get; set; }
+		public bool Numbers { get; set; }
+	}
+}
